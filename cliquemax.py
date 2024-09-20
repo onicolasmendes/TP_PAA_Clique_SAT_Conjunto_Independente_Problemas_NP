828d4bc60c9cf8e4d4ce2c391810ebc44674f783
onicolasmendes <nicolasmela312@gmailcom>
2024-09-19 22:06:05 -0300

    correct branch and bound in max clique and code refactoring complement graph

--- cliquemax.py
+++ cliquemax.py
@@ -1,21 +1,30 @@
 import time
 
-def branch_and_bound(clique_atual, vertices_restantes, melhor_clique, grafo):
+def eCompleta(vertices_restantes): #Verifica se a solução é completa, ou seja, se não há mais vértices a serem adicionados ao clique
     if len(vertices_restantes) == 0:
-        if len(clique_atual) > len(melhor_clique):
-            melhor_clique = clique_atual[:]
-        return
+        return True
+    return False
 
-    for v in vertices_restantes:
-        novos_vertices_restantes = [w for w in vertices_restantes if grafo[v][w] == 1]
-        clique_atual.append(v)
-        branch_and_bound(clique_atual, novos_vertices_restantes, melhor_clique, grafo)
-        clique_atual.pop()
-        
-def clique(grafo, n):
-    melhor_clique = [0]
+def ePromissora(clique_atual, vertices_restantes, melhor_clique): #Verifica se o eventual clique tem potencial de ser melhor que a solução melhor atual
+    if (len(clique_atual)+len(vertices_restantes)) > len(melhor_clique[0]):
+        return True
+    return False
     
-    branch_and_bound([], list(range(n)), melhor_clique, grafo)
+def branch_and_bound(clique_atual, vertices_restantes, melhor_clique, grafo):
+    if eCompleta(vertices_restantes):
+        melhor_clique[0] = clique_atual[:]
+       
+    for v in vertices_restantes:
+        novos_vertices_restantes = [w for w in vertices_restantes if grafo[v][w] == 1] #Adiciona todos os vértices como promissores que possuem aresta com o vértice v, garante que todas as soluções serão consistentes
+        clique_atual.append(v) #Adiciona v ao conjunto de clique atual para eventualmente explorar uma solução que o contém
+        if(ePromissora(clique_atual, novos_vertices_restantes, melhor_clique)): #Poda
+            branch_and_bound(clique_atual, novos_vertices_restantes, melhor_clique, grafo)
+        clique_atual.pop() #Remove v do clique atual para explorar uma eventual solução utilizando o próximo vértice do for
+
+def maxclique(grafo, n):
+    melhor_clique = [[0]] #Solução melhor inicial
+    
+    branch_and_bound([], list(range(n)), melhor_clique, grafo) #Solução inicial = []
     
     return melhor_clique
 
@@ -27,9 +36,9 @@ if __name__ == "__main__":
 
     # Mede o tempo de execução
     start_time = time.time()
-    clique_maximo = clique(grafo, n)
+    clique_maximo = maxclique(grafo, n)
     end_time = time.time()
 
     # Imprime o clique máximo encontrado e o tempo de execução
-    print("Clique máximo:", clique_maximo)
-    print(f"Tempo de execução: {end_time - start_time:.4f} segundos")
+    print("Clique máximo:", clique_maximo[0])
+    print(f"Tempo de execução: {end_time - start_time:.7f} segundos")
--- conjindependente.py
+++ conjindependente.py
@@ -1,13 +1,19 @@
 import time
-from cliquemax import clique
+from cliquemax import maxclique
 
 def complemento_grafo(grafo, n):
-    # Cria o grafo complementar
-    return [[1 - grafo[i][j] if i != j else 0 for j in range(n)] for i in range(n)]
+   for i in range(n):
+       for j in range(n):
+           if i != j: #Desconsiderando a presença de "1" na diagonal principal, conforme orientado pelo professor
+            if grafo[i][j] == 0:
+               grafo[i][j] = 1
+            else:
+               grafo[i][j] = 0
+   return grafo
 
 def conjunto_independente(grafo, n):
     grafo_complemento = complemento_grafo(grafo, n)
-    return clique(grafo_complemento, n)
+    return maxclique(grafo_complemento, n)
 
 if __name__ == "__main__":
     # Lê o grafo de um arquivo
@@ -21,5 +27,5 @@ if __name__ == "__main__":
     end_time = time.time()
 
     # Imprime o conjunto independente máximo encontrado e o tempo de execução
-    print("Conjunto independente máximo:", conjunto_independente_maximo)
+    print("Conjunto independente máximo:", conjunto_independente_maximo[0])
     print(f"Tempo de execução: {end_time - start_time:.4f} segundos")
